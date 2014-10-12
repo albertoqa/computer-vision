3336dbcaa8f744f6ec1ca424985000e9434a9cc1
Alberto Quesada <qa.alberto@gmail.com>
2014-10-12 19:39:10 +0200

    initial commit from local git

---
+++ computer-vision/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  computer-vision
+//
+//  Created by Alberto Quesada Aranda on 12/10/14.
+//  Copyright (c) 2014 Alberto Quesada Aranda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
