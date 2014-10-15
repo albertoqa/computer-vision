425b0ad5291266118c3c7d5b92bb1175326b7700
Alberto Quesada <qa.alberto@gmail.com>
2014-10-16 00:08:14 +0200

    removed some old code

--- src/hybrid.cpp
+++ src/hybrid.cpp
@@ -33,23 +33,6 @@ int reflect(int M, int x)
 
 void GaussFilter(Mat &src, Mat &dst, float sigma, int border_type) {
 	
-	/*int type = src.type();
-	Size size = src.size();
-	dst.create( size, type );
-	
-	Mat temp;
-	temp.create(size, type);
-	
-	Mat xk;
-	createGaussKernel(xk, sigma);
-	
-	float *mask = xk.ptr<float>();
-	int pos = 0;
-	float x1, y1;
-	//cout << xk.rows;
-	
-	float sum = 0;*/
-	
 	dst.create(src.size(), src.type());
 	Mat tmp (src.size(), src.type());
 	float sum, x1, y1;
@@ -59,13 +42,10 @@ void GaussFilter(Mat &src, Mat &dst, float sigma, int border_type) {
 	
 	float *xxk = xk.ptr<float>();
 	
-	//for(int i = 0; i < (sigma * 6) + 1; i++)
-	//	cout << xxk[i] << "    ";
-	
 	int ss = (sigma * 6) + 1;
 	
-	for(int y = 0; y < src.rows; y++){
-		for(int x = 0; x < src.cols; x++){
+	for(int y = 0; y < src.rows; y++) {
+		for(int x = 0; x < src.cols; x++) {
 			sum = 0.0;
 			for(int i = 0; i < ss; i++){
 				y1 = reflect(src.rows, y - i);
@@ -75,10 +55,8 @@ void GaussFilter(Mat &src, Mat &dst, float sigma, int border_type) {
 		}
 	}
 	
-	
-	// along x - direction
-	for(int y = 0; y < src.rows; y++){
-		for(int x = 0; x < src.cols; x++){
+	for(int y = 0; y < src.rows; y++) {
+		for(int x = 0; x < src.cols; x++) {
 			sum = 0.0;
 			for(int i = 0; i < ss; i++){
 				x1 = reflect(src.cols, x - i);
@@ -88,16 +66,6 @@ void GaussFilter(Mat &src, Mat &dst, float sigma, int border_type) {
 		}
 	}
 	
-	
-	
-	
-	//sepFilter2D(_src, _dst, CV_MAT_DEPTH(type), kx, ky, Point(-1,-1), 0, borderType );
-	
-	//usar propiedades de las matrices (producto escalar?)
-	//usar solo una implementacion -> hacerlo por filas, transponer y hcaer lo mismo??
-	
-	
-	
 }
 
 void createGaussKernel(Mat &xk, float sigma) {
@@ -106,7 +74,6 @@ void createGaussKernel(Mat &xk, float sigma) {
 	
 	xk.create(size, 1, CV_32F);
 	float *xxk = xk.ptr<float>();
-	//xk = new float [size];
 	
 	float sum = 0, x, pos;
 	
@@ -127,39 +94,3 @@ void createGaussKernel(Mat &xk, float sigma) {
 	
 }
 
-
-
-/*void dFilterA(Mat &src, Mat &mask, Mat &dst) {
-	
-	Point anchor;
-	double delta;
-	int ddepth;
-	int kernel_size;
-	
-	int c;
-	
-	/// Initialize arguments for the filter
-	anchor = Point( -1, -1 );
-	delta = 0;
-	ddepth = -1;
-	
-	/// Loop - Will filter the image with different kernel sizes each 0.5 seconds
-	int ind = 0;
-	while( true )
-	{
-		c = waitKey(500);
-		/// Press 'ESC' to exit the program
-		if( (char)c == 27 )
-		{ break; }
-		
-		/// Update kernel size for a normalized box filter
-		kernel_size = 3 + 2*( ind%5 );
-		mask = Mat::ones( kernel_size, kernel_size, CV_32F )/ (float)(kernel_size*kernel_size);
-		
-		/// Apply filter
-		filter2D(src, dst, ddepth , mask, anchor, delta, BORDER_DEFAULT );
-		showIM(dst, "filterA");
-		ind++;
-	}
-	
-}*/
