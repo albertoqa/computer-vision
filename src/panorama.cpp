6cd726b623cc5c0947d8016fe48492b600f98e10
Alberto Quesada <qa.alberto@gmail.com>
2014-11-04 11:43:54 +0100

    harris point detection files and point structure

--- src/panorama.cpp
+++ src/panorama.cpp
@@ -1,9 +1,9 @@
 //
-//  corner.cpp
+//  panorama.cpp
 //  computer-vision
 //
-//  Created by Alberto Quesada Aranda on 3/11/14.
+//  Created by Alberto Quesada Aranda on 4/11/14.
 //  Copyright (c) 2014 Alberto Quesada Aranda. All rights reserved.
 //
 
-#include "corner.h"
+#include "panorama.h"
---
+++ src/panorama.h
@@ -0,0 +1,24 @@
+//
+//  panorama.h
+//  computer-vision
+//
+//  Created by Alberto Quesada Aranda on 4/11/14.
+//  Copyright (c) 2014 Alberto Quesada Aranda. All rights reserved.
+//
+
+#ifndef __computer_vision__panorama__
+#define __computer_vision__panorama__
+
+#include <stdio.h>
+#include "opencv2/opencv.hpp"
+using namespace cv;
+
+struct hpoint {
+    
+    int x, y, level;
+    //falta orientación
+    
+};
+
+
+#endif /* defined(__computer_vision__panorama__) */
